1857ec7bb1a444cbca55d9c4bfe2de84192a96b3
Yash Kumar Shukla <69341705+yashshukla0918@users.noreply.github.com>
2022-11-04 15:46:02 +0530

    Upload and Download has changed, in upload card is added

--- src/components/sub-components/Download.js
+++ src/components/sub-components/Download.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import '../component-styles/common.css'
 const Download = () => {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel temporibus culpa asperiores, illum nesciunt aperiam amet tempora corporis placeat reprehenderit fugit quibusdam quisquam ut iusto sint delectus magnam hic reiciendis.
-      </p>
+    <>
+    <div className="container container-style">
+    
     </div>
+    </>
   )
 }
 
--- src/components/sub-components/Upload.js
+++ src/components/sub-components/Upload.js
@@ -1,12 +1,29 @@
+import '../component-styles/common.css'
 import React from 'react'
 
 const Upload = () => {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, nesciunt. Nulla magni quia consequuntur in similique nihil laudantium quod vel natus consectetur animi aspernatur odit, praesentium et quos temporibus accusamus.
-      </p>
+    <>
+    <div className="container container-style">
+    <div className="row my-5"></div>
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="card text-center">
+            <div className="card-body">
+              <div className="card-title"><h4>Chose files to upload</h4></div>
+              <div className='my-2'>
+                <label htmlFor="formFileMultiple" className='form-label'></label>
+                <input type="file" className='form-control' name="" id="formFileMultiple" />
+              </div>
+              <small>You can select multiple file here.</small><br/>
+             <button className='btn btn-outline-success my-3 outline-button-style'>Upload</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-8"></div>
+      </div>
     </div>
+    </>
   )
 }
 
