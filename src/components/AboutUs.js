2426806c2e4e7701933c9da309515426af626fd5
Yash Kumar Shukla <69341705+yashshukla0918@users.noreply.github.com>
2022-11-04 15:56:31 +0530

    About App page added

---
+++ src/components/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutUs = () => {
+
+  return (
+    <>
+    <div className="container">
+      <center><h3>This is about us page</h3></center>
+    </div>
+    </>
+  )
+}
+
+export default AboutUs
