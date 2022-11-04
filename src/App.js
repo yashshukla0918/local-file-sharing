3d87360738bb9a2ae3386887a2ef5d965e03e2d4
Yash Kumar Shukla <69341705+yashshukla0918@users.noreply.github.com>
2022-11-04 16:00:47 +0530

    Nested routing , Error handling and bug fix done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import MainPage from './components/MainPage';
+import Download from "./components/sub-components/Download"
+import Upload from "./components/sub-components/Upload"
+import ErrorPage from './components/ErrorPage'
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+
 } from 'react-router-dom';
-import Download from './components/sub-components/Download';
-import Upload from './components/sub-components/Upload';
+import AboutUs from './components/AboutUs';
+
+
 function App() {
   let hrStyle = {
     "width": "100%",
@@ -20,9 +25,12 @@ function App() {
         <Header title="Local File Sharing"></Header>
         <hr style={hrStyle} />
         <Routes>
-          <Route path="/" element={<MainPage></MainPage>}></Route>
-          <Route path="/sub-components/Download/" element={<Download></Download>}></Route>
-          <Route path="/sub-components/Upload" element={<Upload></Upload>}></Route>
+          <Route path="/" element={<MainPage></MainPage>} >
+            <Route path='/AboutUs' element={<AboutUs/>}></Route>
+          </Route>
+          <Route path='/sub-components/Download' errorElement={<ErrorPage/>}  element={<Download/>}></Route>
+          <Route path='/sub-components/Upload'  element={<Upload/>}></Route>
+          <Route path='*' element={<ErrorPage/>} />
         </Routes>
         <Footer></Footer>
       </Router>
